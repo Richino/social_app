42736c30663c6652c0a632f64d9bb9688a57cb42
Richino Archer <archerrichin@gmail.com>
2023-10-02 23:23:35 -0500

    o

--- server/src/app.ts
+++ server/src/app.ts
@@ -26,7 +26,7 @@ const server = http.createServer(app);
 app.use(express.urlencoded({ extended: true }));
 
 app.use((req: Request, res: Response, next: NextFunction) => {
-	res.header("Access-Control-Allow-Origin", "https://moments.up.railway.app");
+	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
 	res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
 	res.header("Access-Control-Allow-Credentials", "true");
